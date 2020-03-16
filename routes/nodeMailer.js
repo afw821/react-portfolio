62b4a8888d6cc1f942056245f89e8bdb5f8b36ef
Alex Watkins <afw821@gmail.com>
2020-03-15 21:14:16 -0400

    created-react-app and started backend api routes

---
+++ routes/nodeMailer.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const Project = mongoose.model(
+  "Projects",
+  new mongoose.Schema({
+    title: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 50
+    },
+    description: {
+      type: String,
+      required: true,
+      minlength: 10,
+      maxlength: 255
+    },
+    technologies: {
+      type: [String],
+      required: true
+    },
+    deployedUrl: {
+      type: String,
+      required: false
+    },
+    gitHubUrl: {
+      type: String,
+      required: true
+    }
+  })
+);
+
+exports = Project;
