fef3a7a4c20b5ada79439bb49304c34325ca6a5b
Alex Watkins <afw821@gmail.com>
2020-03-15 23:12:01 -0400

    email api route is working

--- routes/nodeMailer.js
+++ routes/nodeMailer.js
@@ -1,2 +1,32 @@
+const nodemailer = require('nodemailer');
+const dotenv = require('dotenv');
 
-module.exports = null;
+dotenv.config();
+
+// function sendEmail(email, name) {
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'afw821@gmail.com',
+    pass: 'txkfnyxernzksbhq'
+  }
+});
+
+const mailOptions = {
+  from: 'afw821@gmail.com',
+  to: 'afw821@gmail.com',
+  subject: 'Thanks',
+  html: '<h1>react-portfolio test email</h1>'
+};
+
+transporter.sendMail(mailOptions, function (error, info) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+    res.send({ status: true, data: info.response })
+  }
+});
+// }
+
+// module.exports = sendEmail;
