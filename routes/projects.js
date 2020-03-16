5413f02c5e08a175574adb8e032a820fbfa4b379
Alex Watkins <afw821@gmail.com>
2020-03-15 21:53:14 -0400

    added api routes to post and get projects

---
+++ routes/projects.js
@@ -0,0 +1,28 @@
+const { Project } = require("../models/project");
+const ash = require("express-async-handler");
+const express = require("express");
+const router = express.Router();
+
+router.get('/', ash(async (req, res) => {
+    const projects = await Project.find()
+        .sort({ title: 'asc' })
+
+    res.send(projects);
+}));
+
+router.post('/', ash(async (req, res) => {
+    const { title, description, technologies, deployedUrl, gitHubUrl } = req.body;
+
+    let project = new Project({
+        title: title,
+        description: description,
+        technologies: technologies,
+        deployedUrl: deployedUrl,
+        gitHubUrl: gitHubUrl
+    });
+    const result = await project.Save();
+
+    res.send({ result, project });
+}));
+
+module.exports = router;
