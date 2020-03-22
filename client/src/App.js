632adf24767471f6d5c1ee36af70178781ed7653
Alex Watkins <afw821@gmail.com>
2020-03-22 13:01:28 -0400

    implemented navbar and routing for components

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import NavBar from './components/navBar'
+import { Route, Switch, Redirect } from 'react-router-dom'
+import Home from './components/home'
+import LoginForm from './components/login'
+import About from './components/about'
+import Contact from './components/contact'
+import Projects from './components/projects'
+import NotFound from './components/notFound'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <NavBar />
+      <div className="container">
+        <Switch>
+          <Route path="/home" component={Home} />
+          <Route path="/login" component={LoginForm} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/projects" component={Projects} />
+          <Route path="/not-found" component={NotFound}></Route>
+          <Redirect from="/" exact to="/home" />
+          <Redirect to="/not-found" />
+        </Switch>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ client/src/components/contact.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+class Contact extends Component {
+  render() {
+    return <h1>Contact Me</h1>
+  }
+}
+
+export default Contact
