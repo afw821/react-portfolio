8e3d068373b507163db0b0052fc554f0e6468369
Alex Watkins <afw821@gmail.com>
2020-04-10 12:21:18 -0400

    Found workaround for CORS issue sending email

--- client/src/services/emailService.js
+++ client/src/services/emailService.js
@@ -3,7 +3,5 @@ import http from "./httpService";
 const apiUrl = "http://localhost:3900/api";
 
 export async function sendMessage(email, message, name) {
-  console.log('obj', email, message, name);
-  const result = await http.post(apiUrl + "/messages", { email: email, message: message, name: name });
-  console.log('result', result);
+  await http.get(apiUrl + `/messages/${name}/${email}/${message}`);
 }
