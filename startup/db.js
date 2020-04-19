43a8045968e4514f0e5926c8199b656ad5cc1603
Alex Watkins <afw821@gmail.com>
2020-04-19 16:22:41 -0400

    change to db js for deployment

--- startup/db.js
+++ startup/db.js
@@ -1,23 +1,16 @@
 const mongoose = require('mongoose');
 module.exports = async function () {
 
-    if (process.env.MONGODB_URI) {
-        console.log('iff so mongo url guy');
+    if (process.env.MONGODB_URI)
+        mongoose.connect(process.env.MONGODB_URI);
+    else {
+        const connection = await mongoose.connect("mongodb://localhost/react_portfolio", { useNewUrlParser: true });
+
+        if (connection)
+            console.log('Connected to MongoDB://react-portfolio');
+        else
+            console.log('Error connecting to MongoDB');
+
     }
-    mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/react_portfolio", { useNewUrlParser: true })
-        .then(function (res) {
-            console.log('Connected to Mongoose');
-        }).catch(error => {
-            console.log('err connecting', err);
-        });
-    // else {
-    //     const connection = await mongoose.connect("mongodb://localhost/react_portfolio", { useNewUrlParser: true });
-
-    //     if (connection)
-    //         console.log('Connected to MongoDB://react-portfolio');
-    //     else
-    //         console.log('Error connecting to MongoDB');
-
-    // }
 
 }
