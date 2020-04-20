f423a051156b27857aa62510aadc50a4a6b6b74b
Alex Watkins <afw821@gmail.com>
2020-04-20 16:30:52 -0400

    remove dotenv

--- models/project.js
+++ models/project.js
@@ -11,7 +11,7 @@ const Project = mongoose.model('Projects', new mongoose.Schema({
         type: String,
         required: true,
         minlength: 5,
-        maxlength: 255
+        maxlength: 500
     },
     technologies: {
         type: [String],
