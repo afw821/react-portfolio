41f5f47a07c3fc1aea466a54975b777a5b62608f
Alex Watkins <afw821@gmail.com>
2020-08-07 11:34:12 -0400

    Updated URI for database migration

--- startup/db.js
+++ startup/db.js
@@ -1,16 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 module.exports = async function () {
+  if (process.env.DB_URI) mongoose.connect(process.env.DB_URI);
+  else {
+    const connection = await mongoose.connect(
+      "mongodb://localhost/react_portfolio",
+      { useNewUrlParser: true }
+    );
 
-    if (process.env.MONGODB_URI)
-        mongoose.connect(process.env.MONGODB_URI);
-    else {
-        const connection = await mongoose.connect("mongodb://localhost/react_portfolio", { useNewUrlParser: true });
-
-        if (connection)
-            console.log('Connected to MongoDB://react-portfolio');
-        else
-            console.log('Error connecting to MongoDB');
-
-    }
-
-}
+    if (connection) console.log("Connected to MongoDB://react-portfolio");
+    else console.log("Error connecting to MongoDB");
+  }
+};
